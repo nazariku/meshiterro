e5089a3177475ee3ccbff00e15849ca15b95a667
EC2 Default User <ec2-user@ip-172-31-26-149.ap-northeast-1.compute.internal>
2023-05-14 09:06:36 +0000

    [Add]画像投稿フォーム

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :post_images, dependent: :destroy
+         #post_imagesをたくさん持っている
+         #dependent: :destroyは1:Nの1側が削除されたとき、N側を全て削除する」という機能
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'homes#top'
   devise_for :users
-  root to: "homes#top"
-  get 'homes/about' => 'homes#about', as: 'about'
+  #resources メソッドは、ルーティングを一括して自動生成してくれる機能
+  #onlyオプションで、生成するルーティングを限定
+  resources :post_images, only: [:new, :create, :index, :show]
+  get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
